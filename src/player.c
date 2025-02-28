23df5618459a40a4c5fbdd9a4de5c5501cf3e93d
isapiev <iba@IbaPC.>
2025-02-28 19:07:33 +0100

    empeche la notif d'or gagner hors ligne d'apparaitre si il est eguale a 0

--- src/player.c
+++ src/player.c
@@ -9,12 +9,14 @@ int damage_click = 1;
 char username[50];
 
 void goldGainOffline(time_t lastSaveTime){
-    char goldEarnedMsg[100];
     time_t currentTime = time(NULL);
     double elapsedTime = difftime(currentTime, lastSaveTime);
     int goldBySec = herosGoldGenBySec();
     int goldEarned = (elapsedTime * goldBySec);
     gold += (int)(goldEarned/10);
-    sprintf(goldEarnedMsg, "%d", goldEarned/10);
-    createNotif(Traduction(GOLD_OFFLINE_MSG),35,1,"assets/ui/notif.png", 1, 3, (SDL_Rect){vw(50), vh(24), vw(40), vh(30)},30, 1.7,2, Traduction(GOLD_OFFLINE_DESC_MSG), goldEarnedMsg);
+    if (goldEarned != 0){
+        char goldEarnedMsg[100];
+        sprintf(goldEarnedMsg, "%d", goldEarned/10);
+        createNotif(Traduction(GOLD_OFFLINE_MSG),35,1,"assets/ui/notif.png", 1, 3, (SDL_Rect){vw(50), vh(24), vw(40), vh(30)},30, 1.7,2, Traduction(GOLD_OFFLINE_DESC_MSG), goldEarnedMsg);
+    }
 }
